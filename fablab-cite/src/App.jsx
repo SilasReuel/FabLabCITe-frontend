3e7828b3f97b2aecf00672bfcafcee8678882a69
SilasReuel <silas.reuel@unesp.br>
2024-12-13 16:59:36 -0300

    feat: inicializa o projeto React com estrutura padrão chore(config): adiciona arquivos iniciais do projeto React

---
+++ fablab-cite/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!!!</h1>
+      <p>Este é o futuro site da FabLab da UNESP-BAURU</p>
+    </>
+  )
+}
+
+export default App
