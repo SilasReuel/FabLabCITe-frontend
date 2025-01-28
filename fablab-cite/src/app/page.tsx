b3a1625aaa91a1bbd4a6b9e52582de16b90198d2
leopires1 <leonardo-pires.silva@unesp.br>
2025-01-28 12:59:07 -0300

    feat(Crud): Adicionando métodos para consumir api de usuários.
    Cria arquivo api, onde foi desenvolvido os métodos de crud para a entidade usuário. Cria contexto de usuário que será utilizado futuramente.

--- fablab-cite/src/app/page.tsx
+++ fablab-cite/src/app/page.tsx
@@ -1,5 +1,11 @@
+import { UsersService } from "@/services/Users/users";
+
 export default function Home() {
+  UsersService.fetchUsers()
+
   return (
-    <></>
+    <>
+      <h1>Usuários</h1>
+    </>
   );
 }
